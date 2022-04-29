84f2cd3475bbdb36f1d8890008975916b8c2f5ad
Adebambo Adenuga <35952059+Pils36@users.noreply.github.com>
2022-04-29 10:38:47 +0100

    adjustment

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(fileupload({ useTempFiles: true }));
 
 // Declare Routes
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/v1/farm', require('./routes/users/farm'));
 
 app.use('/', (req, res) => {
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // Make database connection
 module.exports = mongoose.connect(
-	process.env.APP_ENV == 'local' ? process.env.DB_CONNECTION_DEV : process.env.DB_CONNECTION_PROD,
+	process.env.NODE_ENV == 'local' ? process.env.DB_CONNECTION_DEV : process.env.DB_CONNECTION_PROD,
 	(error, result) => {
 		if (error) return console.log(error);
 		console.log('Connected to Database');
