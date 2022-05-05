fb1373ff983829c0157f3aef55689370a67d463c
Adebambo Adenuga <35952059+Pils36@users.noreply.github.com>
2022-05-05 11:58:53 +0100

    endpoint update

--- src/controllers/FarmController.js
+++ src/controllers/FarmController.js
@@ -2,14 +2,14 @@ const FarmLocation = require('../models/FarmLocation');
 
 const fetchFarmLocation = async (req, res) => {
 	try {
-		const getLocation = await FarmLocation.find({ farmType: req.query.farmType, userId: req.query.userId }).sort({
+		const getLocation = await FarmLocation.find({ userId: req.query.userId }).sort({
 			farmType: 'ASC'
 		});
 
 		if (getLocation.length == 0)
 			return res.status(200).json({
 				data: [],
-				message: `You have no location set for your ${req.query.farmType} farm`
+				message: `You have no farm location saved yet`
 			});
 
 		res.status(200).json({
@@ -52,4 +52,26 @@ const saveFarmLocation = async (req, res) => {
 	}
 };
 
-module.exports = { fetchFarmLocation, saveFarmLocation };
+const getSpecificFarm = async (req, res) => {
+	try {
+		const data = await await FarmLocation.findOne({ _id: req.params._id });
+
+		if (!data)
+			return res.status(404).json({
+				data: [],
+				message: `Farm location not found`
+			});
+
+		res.status(200).json({
+			data: data,
+			message: `Success`
+		});
+	} catch (error) {
+		res.status(401).json({
+			data: [],
+			message: error.message
+		});
+	}
+};
+
+module.exports = { fetchFarmLocation, saveFarmLocation, getSpecificFarm };
--- src/routes/users/farm.js
+++ src/routes/users/farm.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { fetchFarmLocation, saveFarmLocation } = require('../../controllers/FarmController');
+const { fetchFarmLocation, saveFarmLocation, getSpecificFarm } = require('../../controllers/FarmController');
 
 router.get('/fetch-location', fetchFarmLocation);
 router.post('/save-location', saveFarmLocation);
+router.get('/get-specific-location/:_id', getSpecificFarm);
 
 module.exports = router;
